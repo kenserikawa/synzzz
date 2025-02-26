6d0089734b92cfbc0a6bcdee8c9b016b7bb1005b
KenSerikawa <kenserikawalucio@gmail.com>
2025-02-27 00:35:31 +0100

    encapsule const on files

---
+++ util/waveforms.js
@@ -0,0 +1,6 @@
+export const waveforms = [
+    { icon: '∿', name: 'Sine', value: 'sine' },
+    { icon: '▇', name: 'Square', value: 'square' },
+    { icon: '◺', name: 'Triangle', value: 'triangle' },
+    { icon: '◿', name: 'Sawtooth', value: 'sawtooth' }
+];
