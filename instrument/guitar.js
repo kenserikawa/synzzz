d43eadaadacb9fee877144313973253b4eaf0a80
KenSerikawa <kenserikawalucio@gmail.com>
2025-03-06 15:48:54 +0100

    add basic guitar node

--- instrument/guitar.js
+++ instrument/guitar.js
@@ -1,16 +1,45 @@
+// Guitar.js
 import Chordophone from "./type/chordophone.js";
 
 export default class Guitar extends Chordophone {
     constructor() {
         super("Guitar");
     }
-  
-    play() {
+
+    play(frequency, duration) {
         console.log("Strumming guitar strings.");
-        this.pluck();
+        this.playSound(frequency, duration);
     }
-  
-    tune() {
-        console.log("Tuning guitar strings.");
+
+    playSound(frequency, duration) {
+        const distortion = this.audioContext.createWaveShaper();
+        distortion.curve = this.makeDistortionCurve(400);
+        distortion.oversample = '4x';
+
+        const oscillator = this.audioContext.createOscillator();
+        oscillator.type = 'triangle';
+        oscillator.frequency.setValueAtTime(frequency, this.audioContext.currentTime);
+
+        const gainNode = this.audioContext.createGain();
+        oscillator.connect(distortion);
+        distortion.connect(gainNode);
+        gainNode.connect(this.gainNode);
+
+        this.applyEnvelope(gainNode, 0.05, 0.2, 0.5, 0.3, 0.4);
+
+        oscillator.start();
+        oscillator.stop(this.audioContext.currentTime + duration + 0.05 + 0.2 + 0.5 + 0.4);
+    }
+
+    makeDistortionCurve(amount) {
+        let k = typeof amount === 'number' ? amount : 50;
+        let n_samples = 44100;
+        let curve = new Float32Array(n_samples);
+        let deg = Math.PI / 180;
+        for (let i = 0; i < n_samples; ++i) {
+            let x = i * 2 / n_samples - 1;
+            curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+        }
+        return curve;
     }
 }
--- instrument/type/chordophone.js
+++ instrument/type/chordophone.js
@@ -1,3 +1,4 @@
+// chordophone.js
 import Instrument from "../interface/instrument.js";
 
 export default class Chordophone extends Instrument {
@@ -8,7 +9,29 @@ export default class Chordophone extends Instrument {
         }
     }
 
-    pluck() {
-        console.log(`${this.name} strings are being plucked.`);
+    applyEnvelope(gainNode, attack, decay, sustain, sustainLevel, release) {
+        const now = this.audioContext.currentTime;
+        gainNode.gain.cancelScheduledValues(now);
+        gainNode.gain.setValueAtTime(0, now);
+
+        gainNode.gain.linearRampToValueAtTime(1, now + attack);
+        gainNode.gain.exponentialRampToValueAtTime(sustainLevel, now + attack + decay);
+        gainNode.gain.setValueAtTime(sustainLevel, now + attack + decay + sustain);
+        gainNode.gain.linearRampToValueAtTime(0, now + attack + decay + sustain + release);
+    }
+
+    play(frequency, duration) {
+        const oscillator = this.audioContext.createOscillator();
+        oscillator.type = 'sine';
+        oscillator.frequency.setValueAtTime(frequency, this.audioContext.currentTime);
+
+        const gainNode = this.audioContext.createGain();
+        oscillator.connect(gainNode);
+        gainNode.connect(this.gainNode);
+
+        this.applyEnvelope(gainNode, 0.1, 0.2, 0.5, 0.3, 0.3); 
+
+        oscillator.start();
+        oscillator.stop(this.audioContext.currentTime + duration + 0.1 + 0.2 + 0.5 + 0.3);
     }
 }
